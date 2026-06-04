5cf5f7146245814158be4198ffa31da1ae6e4c3d
Hemant Prajapat <prajapathemant564@gmail.com>
2026-06-04 16:10:16 +0530

    feat: complete expense tracker with responsive charts, cleaned structure, and fixed timezone handling

--- Backend/src/config/db.d.ts
+++
@@ -1,5 +0,0 @@
-import Database from 'better-sqlite3';
-declare const db: Database.Database;
-export declare const initDB: () => void;
-export default db;
-//# sourceMappingURL=db.d.ts.map
--- Backend/src/controllers/expenseController.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response } from 'express';
-export declare const expenseController: {
-    getExpenses: (req: Request, res: Response) => void;
-    getExpenseSummary: (req: Request, res: Response) => void;
-    createExpense: (req: Request, res: Response) => any;
-    updateExpense: (req: Request, res: Response) => any;
-    deleteExpense: (req: Request, res: Response) => any;
-};
-//# sourceMappingURL=expenseController.d.ts.map
